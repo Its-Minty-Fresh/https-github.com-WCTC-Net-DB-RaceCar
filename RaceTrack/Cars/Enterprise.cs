cdf2307890fa907c06a6a109f87e333b3649a257
Matthew Kapitan <mkapitan@my.wctc.edu>
2020-03-09 21:06:28 -0500

    Added Driver Mr. Spock, car the Starship Enterprise, and added the shop engine method

---
+++ RaceTrack/Cars/Enterprise.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    public class Enterprise : RaceCar
+    {
+        public Enterprise()
+        {
+            Name = "The Starship Enterprise";
+            TopSpeed = 100;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The {Name} engages warp drive!");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The {Name} doesn't stop on a dime Jim!!");
+            base.Brake();
+        }
+    }
+}
